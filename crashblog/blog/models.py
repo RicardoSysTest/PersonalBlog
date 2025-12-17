cb1446337669483db8143ad5a4ea2f968159887f
uif52594 <david.yepez.espinosa@continental-corporation.com>
2025-12-16 20:48:04 -0600

    Add fileter for Categories

--- crashblog/blog/models.py
+++ crashblog/blog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
 
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -13,6 +23,9 @@ class Post(models.Model):
     #Sort the Data to display first the most recent
     class Meta:
         ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
